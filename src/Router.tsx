ba7a69983c00b1af40961f59e29540a82f7ec6ef
Magno Brandao <magnoboc@gmail.com>
2023-10-29 19:15:05 -0300

    feat: added search page incomplete

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import { Details } from './pages/details/Details'
 
 import { Home } from './pages/home/Home'
+import { Search } from './pages/search/Search'
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/details" element={<Details />} />
+            <Route path="/search" element={<Search />} />
         </Routes>
     )
 }
--- src/shared/components/lastNews/LastNews.tsx
+++ src/shared/components/lastNews/LastNews.tsx
@@ -2,92 +2,63 @@
 import {
     Box,
     Heading,
-    Image,
     Text,
     Divider,
-    HStack,
-    Tag,
     Wrap,
     WrapItem,
-    SpaceProps,
     Container,
 
 } from '@chakra-ui/react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-interface IBlogTags {
-    tags: Array<string>
-    marginTop?: SpaceProps['marginTop']
+
+interface Data {
+    name: string;
+    is_potentially_hazardous_asteroid: boolean;
+    kilometers_per_hour: string;
 }
-
-interface Props {
-    marginTop?: number
-    tags: any[]
-}
-
-const BlogTags = (props: Props) => {
-    const { marginTop = 0, tags } = props
-
-    return (
-        <HStack spacing={2} marginTop={marginTop}>
-            {tags.map((tag) => {
-                return (
-                    <Tag size={'md'} variant="solid" colorScheme="orange" key={tag}>
-                        {tag}
-                    </Tag>
-                )
-            })}
-        </HStack>
-    )
-}
-
 
 const LastNews = () => {
+    const [data, setData] = useState<Data | null>(null);
+
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=Ie25M3zSZBhfrQdesyHL1O7jQHJQwWHp5voA4BCE')
+            .then(response => {
+                const dataFromApi = response.data.near_earth_objects;
+                setData(dataFromApi);
+                console.log(dataFromApi)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <Container maxW={'4xl'} color="white">
-
             <Heading as="h2" marginTop="12">
                 Latest News
             </Heading>
             <Divider marginTop="5" />
             <Wrap spacing="30px" marginTop="5">
                 <WrapItem width={{ base: '100%' }}>
-                    <Box w="100%">
-                        <Box borderRadius="lg" overflow="hidden">
-                            <Box textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                                <Image
-                                    transform="scale(1.0)"
-                                    src={
-                                        'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                                    }
-                                    alt="some text"
-                                    objectFit="contain"
-                                    width="100%"
-                                    transition="0.3s ease-in-out"
-                                    _hover={{
-                                        transform: 'scale(1.05)',
-                                    }}
-                                />
-                            </Box>
-                        </Box>
-                        <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
+                    <Box w="100%" borderRadius="lg" overflow="hidden">
                         <Heading fontSize="xl" marginTop="2">
                             <Text textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                                Some blog title
+                                {data?.name}
                             </Text>
                         </Heading>
                         <Text as="p" fontSize="md" marginTop="2">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry&apos;s standard dummy text ever since the
-                            1500s, when an unknown printer took a galley of type and scrambled it to
-                            make a type specimen book.
+                            is_potentially_hazardous_asteroid: {data?.is_potentially_hazardous_asteroid?.toString()}
                         </Text>
-
+                        <Text as="p" fontSize="md" marginTop="2">
+                            kilometers_per_hour: {data?.kilometers_per_hour}
+                        </Text>
                     </Box>
                 </WrapItem>
             </Wrap>
-
         </Container>
-    )
-}
+    );
+};
 
-export default LastNews
+export default LastNews;
